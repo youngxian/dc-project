401d03b5c871b56c0dfd1eabb7f433c119d1d006
Jeremiah Chukwuka <jeremiahchukwu@gmail.com>
2021-12-05 09:47:30 +0100

    settings

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import HomeView, WorkExprienceView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('work/exprience', WorkExprienceView.as_view(), name='work_exprience')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
