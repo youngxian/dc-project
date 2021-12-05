723a97ea8ddfd1d1fe3f87b385c195bffd47522c
Jeremiah Chukwuka <jeremiahchukwu@gmail.com>
2021-12-05 09:03:52 +0100

    added sqlite database config

--- aws_city/settings.py
+++ aws_city/settings.py
@@ -90,13 +90,20 @@ WSGI_APPLICATION = 'aws_city.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': config('DATABASE_NAME'),
+    #     'USER':  config('DATABASE_USER'),
+    #     'PASSWORD': config('DATABASE_PASSWORD'),
+    #     'HOST':  config('DATABASE_HOST'),
+    #     'PORT': config('DATABASE_PORT')
+    # }
+
+    # Mysqlite
+
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DATABASE_NAME'),
-        'USER':  config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST':  config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
